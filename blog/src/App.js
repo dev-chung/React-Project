6526aebf735d8802bcb22ee084e94c3c3e288fb9
cndgus6413 <cndgus6413@naver.com>
2022-07-01 23:25:15 +0900

    React-props

--- blog/src/App.js
+++ blog/src/App.js
@@ -5,6 +5,7 @@ function App() {
   //State 장점: 데이터가 변경 되어도 새로고침되지 않고 재렌더링되어 반영
   //ES6 destructuring
   let [글제목, 글제목변경] = useState(['남자 코트 추천', '새해복 많이 받으세요', '한 해의 마지막']);
+  let [날짜, 날짜변경] = useState(['2월 27일', '1월 1일', '12월 31일',])
   let [좋아요, 좋아요변경] = useState(0);
 
   function 제목바꾸기() {
@@ -20,21 +21,35 @@ function App() {
       <div className='list'>
         <button onClick={제목바꾸기}>버튼</button>
         <h4>{글제목[0]} <span onClick={() => { 좋아요변경(좋아요 + 1) }}>💜</span> {좋아요} </h4>
-        <p>2월 17일 발행</p>
+        <p>{날짜[0]}</p>
         <hr />
       </div>
       <div className='list'>
         <h4>{글제목[1]}</h4>
-        <p>1월 1일 발행</p>
+        <p>{날짜[1]}</p>
         <hr />
       </div>
       <div className='list'>
         <h4>{글제목[2]}</h4>
-        <p>12월 31일 발행</p>
+        <p>{날짜[2]}</p>
         <hr />
       </div>
+
+      <Modal title={글제목[0]} date={날짜[0]} />
+
     </div>
   );
+}
+
+//컴포넌트
+function Modal(props) {
+  return (
+    <div className='modal'>
+      <h2> {props.title} </h2>
+      <p>{props.date}</p>
+      <p>상세내용</p>
+    </div>
+  )
 }
 
 export default App;
